1f64cccf520c8f6cb7f1eb142336412cb7b03bcb
wataru <ltomu.web@gmail.com>
2023-02-21 23:59:38 +0900

    feat: userモデルのテスト追加

---
+++ backend/spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    name { 'neumann' }
+    email { 'neumann@neumann.com' }
+  end
+end
--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.build(:user) }
+
+  it '有効なファクトリを持つこと' do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
 end
