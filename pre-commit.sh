ff8f8d9afd82e419e4b38bbd374ff87ff52f7afb
wataru <ltomu.web@gmail.com>
2024-01-11 01:34:47 +0900

    feat: commit時にコンテナ内のrubcopが実行される
    設定方法
    cp pre-commit.sh .git/hooks/pre-commit
    してからコンテナを起動してコードを編集してcommitすると
    変更ファイル（backendフォルダ下.rb）に対してrubcopが実行されます
    
    def badName
      if something
        test
        end
    end
    
    上記のファイルを追加してcommitしようとするとエラーになります

---
+++ pre-commit.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# ファイルパスの取得
+file_paths=$(git diff --cached --name-only --diff-filter=AM | grep '\.rb$' | cut -d'/' -f2-)
+
+# ファイルが存在するかチェック
+if [ -n "$file_paths" ]; then
+  echo '---Rubocopで警告がないかチェック---'
+  echo "$file_paths" | xargs docker compose -f docker-compose.backend.yml -p backend exec -T backend rubocop --display-only-fail-level-offenses
+fi
