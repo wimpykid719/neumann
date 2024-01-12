21e95832c4d6e5f814a579ba5edd56d0efbbbfff
wataru <ltomu.web@gmail.com>
2024-01-12 12:01:28 +0900

    fix: rubocop.ymlに記載された除外設定を個別にチェックした際も適応する
    pre-commitのコマンドで--force-exclusionを指定して個別ファイル時も
    除外設定をあてる
    
    https://ryoito.jp/articles/e0f9bb0e

--- pre-commit.sh
+++ pre-commit.sh
@@ -6,5 +6,5 @@ file_paths=$(git diff --cached --name-only --diff-filter=AM | grep '\.rb$' | cut
 # ファイルが存在するかチェック
 if [ -n "$file_paths" ]; then
   echo '---Rubocopで警告がないかチェック---'
-  echo "$file_paths" | xargs docker compose -f docker-compose.backend.yml -p backend exec -T backend rubocop --display-only-fail-level-offenses
+  echo "$file_paths" | xargs docker compose -f docker-compose.backend.yml -p backend exec -T backend rubocop --force-exclusion --display-only-fail-level-offenses
 fi
