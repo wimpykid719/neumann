6cae6f66cecc940922cdfe93622e1f99ca03b7f7
wataru <ltomu.web@gmail.com>
2024-01-22 11:53:28 +0900

    feat: csrf 対策
    headerにX-Requested-With: XMLHttpRequestという文字列が含まれている事を確認する事で
    csrf対策をしている。
    
    JavaScriptによって作成されたリクエストの場合
    X-Requested-With: XMLHttpRequest を簡単に含められるがJavaScriptからのリクエスト
    だと同一オリジンの設定で弾かれる
    
    Formからのリクエストだと同一オリジンの制限を受けないがheaderを
    カスタムする事が出来ないためformからのリクエストは弾かれる
    
    そのためcsrf対策になる

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::API
+  # CSRF対策
+  before_action :verify_xhr_request
+
   include ActionController::Cookies
   include UserAuth::AuthenticateService
+
+  private
+
+  def verify_xhr_request
+    return if request.xhr?
+
+    render status: :forbidden, json: { status: :forbidden, error: 'Forbidden' }
+  end
+
+  # Internal Server Error
+  def response_internal_server_error(msg = 'Internal Server Error')
+    render status: :internal_server_error, json: { status: :internal_server_error, error: msg }
+  end
 end
--- backend/spec/requests/api/v1/users_controller_spec.rb
+++ backend/spec/requests/api/v1/users_controller_spec.rb
@@ -4,17 +4,18 @@ RSpec.describe Api::V1::UsersController do
   include TokenConcern
 
   let(:user) { FactoryBot.create(:user, current_token_version: new_token_version) }
-  let!(:access_token) { user.generate_refresh_token }
+  let!(:access_token) { user.generate_access_token }
+  let!(:headers) { { headers: { 'X-Requested-With' => 'XMLHttpRequest', Authorization: "Bearer #{access_token.token}" } } }
 
   describe 'GET #index' do
     context '成功 ステータス200' do
       it '認証成功' do
-        get api_v1_users_path, headers: { Authorization: "Bearer #{access_token.token}" }
+        get api_v1_users_path, **headers
         expect(response).to have_http_status(:ok)
       end
 
       it '認証ユーザが返る' do
-        get api_v1_users_path, headers: { Authorization: "Bearer #{access_token.token}" }
+        get api_v1_users_path, **headers
         json = response.parsed_body
         expect(json['name']).to eq('neumann')
       end
