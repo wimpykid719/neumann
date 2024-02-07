cdab4656ed504a925878a062b91cf63dad57d92f
wataru <ltomu.web@gmail.com>
2024-02-07 22:28:58 +0900

    fix: ログインフォームのバリデーション修正
    最大入力値のバリデーションを追加

--- frontend/neumann-client/src/lib/zod-schema/login-validation.ts
+++ frontend/neumann-client/src/lib/zod-schema/login-validation.ts
@@ -4,6 +4,6 @@ import { z } from 'zod'
 export type LoginValidation = z.infer<typeof loginValidationSchema>
 
 export const loginValidationSchema = z.object({
-  email: z.string().min(1, validation.emailMin).email(validation.emailIncorect),
-  password: z.string().min(8, validation.passwordMin),
+  email: z.string().min(1, validation.emailMin).max(255, validation.emailMax).email(validation.emailIncorect),
+  password: z.string().min(8, validation.passwordMin).max(72, validation.passwordMax),
 })
