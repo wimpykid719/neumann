ab2003fd339cd75cad01dceb6b4335ac7f5c8f75
wataru <ltomu.web@gmail.com>
2024-02-14 21:27:27 +0900

    chore: jestの参照パス修正

--- frontend/neumann-client/jest.config.ts
+++ frontend/neumann-client/jest.config.ts
@@ -86,7 +86,7 @@ const config: Config = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '^@/src/(.*)$': '<rootDir>/src/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
