4c5d7869ef49b96a13fe210ab5c767d03c649316
wataru <ltomu.web@gmail.com>
2024-02-22 20:31:43 +0900

    feat: ヘッダーコンポーネントを追加
    アバターをクリックするとメニューが開く
    メニュー外をクリックすると閉じる仕様
    
    下記の記事を参考に作成
    https://qiita.com/G4RDSjp/items/58364a6655d4968a90d9

---
+++ frontend/neumann-client/src/components/common/Header.tsx
@@ -0,0 +1,91 @@
+'use client'
+import app from '@/text/app.json'
+import { motion } from 'framer-motion'
+import { useEffect, useRef, useState } from 'react'
+import BizRankIcon from './icon/BizRankIcon'
+import HurtIcon from './icon/HurtIcon'
+import LogoutIcon from './icon/LogoutIcon'
+import SettingsIcon from './icon/SettingsIcon'
+
+export default function Header() {
+  const [isOpen, setOpen] = useState(false)
+  const menuRef = useRef<HTMLDivElement>(null)
+  const documentClickHandler = useRef<(e: MouseEvent) => void>(() => {})
+  const variants = {
+    visible: { top: '64px', opacity: 1, scale: 1 },
+    hidden: { opacity: 0, bottom: 0, scale: 0.5 },
+  }
+
+  useEffect(() => {
+    documentClickHandler.current = e => {
+      if (e.target === null) return
+      // メニューの内側をクリックした場合何もしない、as Nodeに関してはクリックされる要素の型を推論するのは難しいため
+      if (menuRef.current?.contains(e.target as Node)) return
+
+      setOpen(false)
+      removeDocumentClickHandler()
+    }
+  }, [])
+
+  const handleClick = () => {
+    if (isOpen) return
+
+    setOpen(true)
+    document.addEventListener('click', documentClickHandler.current)
+  }
+
+  const removeDocumentClickHandler = () => {
+    document.removeEventListener('click', documentClickHandler.current)
+  }
+
+  return (
+    <section className='flex justify-between py-7'>
+      <div>
+        <h1 className='text-lg font-bold'>
+          <BizRankIcon width={32} height={32} className='mr-2 inline' />
+          {app.title}
+        </h1>
+      </div>
+      <div className='relative'>
+        <button
+          onClick={handleClick}
+          className='w-12 h-12 rounded-lg shadow sub-bg-color text-xs font-medium text-center dark:border dark:border-gray-600'
+        >
+          (,,0‸0,,)
+        </button>
+        {isOpen && (
+          <motion.div
+            initial={{ right: 0, opacity: 0, scale: 0.3 }}
+            animate={isOpen ? 'visible' : 'hidden'}
+            variants={variants}
+            transition={{ ease: 'easeOut', duration: 0.2 }}
+            className='absolute rounded-lg shadow sub-bg-color dark:border dark:border-gray-600 min-w-56'
+            ref={menuRef}
+          >
+            <div className='font-bold p-3'>ユーザ名</div>
+            <ul>
+              <li className='flex items-center p-3 cursor-pointer hover:bg-gray-500'>
+                <span className='inline-flex items-center w-7'>
+                  <HurtIcon />
+                </span>
+                いいねした本
+              </li>
+              <li className='flex items-center p-3 cursor-pointer hover:bg-gray-500'>
+                <span className='inline-flex items-center w-7'>
+                  <SettingsIcon />
+                </span>
+                アカウント設定
+              </li>
+              <li className='flex items-center p-3 cursor-pointer hover:bg-gray-500 rounded-b-lg'>
+                <span className='inline-flex items-center w-7'>
+                  <LogoutIcon />
+                </span>
+                ログアウト
+              </li>
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </section>
+  )
+}
