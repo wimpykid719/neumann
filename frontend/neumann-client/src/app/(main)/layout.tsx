ca8ca8180db76f0d6b257ab4aaff7156688501d5
wataru <ltomu.web@gmail.com>
2024-02-22 20:34:53 +0900

    chore: トップページをグループ化
    ヘッダーをログイン・サインアップページでは表示させたくないので
    グループで分けトップページのみの共通レイアウトを追加しました

---
+++ frontend/neumann-client/src/app/(main)/layout.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/common/Header'
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <main className='m-auto xl:max-w-5xl'>
+      <Header />
+      {children}
+    </main>
+  )
+}
