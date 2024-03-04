7524c7d4908a3c6cf370c545a43a84bd7fb80b3d
wataru <ltomu.web@gmail.com>
2024-03-04 21:16:12 +0900

    fix: 30分後のユーザ詳細取得で認証エラーでログアウト状態となる
    30分後アクセストークンの有効期限が切れて
    別のタブからフォーカスが移った際にswrの再検証でサイレントリフレッシュ
    が走らずにユーザ詳細を取得する処理が走る
    そのため再検証が走らないメソッドに変更

--- frontend/neumann-client/src/hooks/useUserInitialFetch.ts
+++ frontend/neumann-client/src/hooks/useUserInitialFetch.ts
@@ -5,14 +5,14 @@ import { AccessToken } from '@/types/accessToken'
 import { ToastType } from '@/types/toast'
 import { toastStatus } from '@/utils/toast'
 import { useEffect } from 'react'
-import useSWR from 'swr'
+import useSWRImmutable from 'swr/immutable'
 
 export const useUserInitialFetch = (
   accessToken: AccessToken,
   showToast: (message: string, type: ToastType) => void,
 ) => {
   const { user, setUser } = useUser()
-  const { data, isLoading } = useSWR(accessToken || null, accessToken => getUser(accessToken))
+  const { data, isLoading } = useSWRImmutable(accessToken || null, accessToken => getUser(accessToken))
 
   useEffect(() => {
     if (data instanceof FetchError) {
