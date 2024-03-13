0ed8a30019a4c5682d1190278c03f1a66398a188
wataru <ltomu.web@gmail.com>
2024-03-13 15:51:08 +0900

    fix: 不要なテンプレートリテラル

--- frontend/neumann-client/src/lib/wrappedFeatch/userRequest.ts
+++ frontend/neumann-client/src/lib/wrappedFeatch/userRequest.ts
@@ -4,7 +4,7 @@ import { User } from '@/types/user'
 type Response = User
 
 export async function getUser(accessToken: string) {
-  return await fetch.get<Response>(`/api/v1/users/`, {
+  return await fetch.get<Response>('/api/v1/users/', {
     headers: { Authorization: `Bearer ${accessToken}` },
     credentials: 'include',
   })
