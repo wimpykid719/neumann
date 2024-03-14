e4f3f97d34b3e3b02ad0ec03f4e697ba32962307
wataru <ltomu.web@gmail.com>
2024-03-14 19:42:23 +0900

    fix: [ActionDispatch::HostAuthorization::DefaultResponseApp] Blocked hosts: backend:8080
    コンテナ間で通信できるようになったが、railsサーバからブロックされていたので、ホストを許可する
    設定を追加しました

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -17,6 +17,9 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # フロントエンドからのサーバサイドからのリクエストを許可する
+  config.hosts << ENV.fetch('ALLOW_HOST_PORT')
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
