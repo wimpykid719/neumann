0e2589f02fd6d711ffb6a8245e90cb5f87936397
wataru <ltomu.web@gmail.com>
2024-03-25 19:48:09 +0900

    fix: サインアップ時ユーザ名のバリデーション（フロントエンド）

--- frontend/neumann-client/src/lib/zodSchema/signupValidation.ts
+++ frontend/neumann-client/src/lib/zodSchema/signupValidation.ts
@@ -4,7 +4,11 @@ import { z } from 'zod'
 export type SignupValidation = z.infer<typeof SignupValidationSchema>
 
 export const SignupValidationSchema = z.object({
-  name: z.string().min(1, validation.nameMin).max(50, validation.nameMax),
+  name: z
+    .string()
+    .min(1, validation.nameMin)
+    .regex(/^[\w\-]+$/, { message: validation.userName })
+    .max(50, validation.nameMax),
   email: z.string().min(1, validation.emailMin).max(255, validation.emailMax).email(validation.emailIncorect),
   password: z.string().min(8, validation.passwordMin).max(72, validation.passwordMax),
 })
