a5883a8829e57c018d856cbd8199f18819b2c342
wataru <ltomu.web@gmail.com>
2024-03-31 21:15:16 +0900

    fix: htmlタグのlangをjaに変更

--- frontend/neumann-client/src/app/layout.tsx
+++ frontend/neumann-client/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
+    <html lang='ja'>
       <body className={inter.className}>
         <AccessTokenProvider>
           <UserProvider>
