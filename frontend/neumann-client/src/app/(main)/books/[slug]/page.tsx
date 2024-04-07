eae0edce37ac265320d19ae2165f12dbe43a856a
wataru <ltomu.web@gmail.com>
2024-04-07 19:00:07 +0900

    feat: 書籍の詳細ページ
    ビルド時に書籍詳細ページを生成するように設定
    説明に含まれる改行がrailsのxss対策で\\nに置き換えられて返るので
    それを元の改行に戻すメソッドを追加

---
+++ frontend/neumann-client/src/app/(main)/books/[slug]/page.tsx
@@ -0,0 +1,75 @@
+import Card from '@/components/common/Card'
+import InfoCard from '@/components/detail/InfoCard'
+import { FetchError } from '@/lib/errors'
+import { getBook, getBooks } from '@/lib/wrappedFeatch/requests/book'
+import error from '@/text/error.json'
+import { FIRST_PAGE } from '@/utils/page'
+import { range } from '@/utils/range'
+import { replaceNewlines } from '@/utils/replaceNewlines'
+
+type SlugsProps = {
+  slug: string
+}
+
+export const generateStaticParams = async () => {
+  const getBooksSlugs = async (page: number) => {
+    const res = await getBooks(page)
+    if (res instanceof FetchError) {
+      console.error(error.failedBooksFetch)
+      return
+    }
+
+    return res.books.map(book => book.id)
+  }
+
+  const res = await getBooks()
+
+  if (res instanceof FetchError) {
+    console.error(error.failedBooksFetch)
+    return
+  }
+
+  const pages = range(FIRST_PAGE, res.pages.last).map(num => num)
+  const allBookIds = await (async pages => {
+    return await Promise.all(
+      pages.map(async page => {
+        return getBooksSlugs(page)
+      }),
+    )
+  })(pages)
+
+  return allBookIds.reduce((slugs: SlugsProps[], pageBookIds) => {
+    if (pageBookIds) {
+      return [...slugs, ...pageBookIds.map(id => ({ slug: id.toString() }))]
+    }
+    return slugs
+  }, [])
+}
+
+export default async function Detail({ params }: { params: SlugsProps }) {
+  const res = await getBook(params.slug)
+
+  if (res instanceof FetchError) {
+    console.error(error.failedBookFetch)
+    return
+  }
+
+  return (
+    <section className='flex flex-wrap gap-7'>
+      <Card title={res.title} img_url={res.img_url} ranking={res.ranking} detail={false} />
+      <div className='space-y-8 flex-1'>
+        <h2 className='font-bold text-lg'>{res.title}</h2>
+        <article className='whitespace-pre-wrap'>{replaceNewlines(res.description)}</article>
+      </div>
+      <InfoCard
+        id={res.id}
+        associate_url={res.associate_url}
+        score={res.score}
+        likes={120}
+        page={res.page}
+        launched={res.launched}
+        publisher={res.publisher}
+      />
+    </section>
+  )
+}
---
+++ frontend/neumann-client/src/utils/replaceNewlines.ts
@@ -0,0 +1,1 @@
+export const replaceNewlines = (msg: string) => msg.replace(/\\n/g, '\n')
