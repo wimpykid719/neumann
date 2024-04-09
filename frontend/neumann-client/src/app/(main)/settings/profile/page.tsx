f07ca685156407e46e2835ed0376a7713446d71a
wataru <ltomu.web@gmail.com>
2024-04-09 22:51:37 +0900

    refactor: importの順番

--- frontend/neumann-client/src/app/(main)/settings/profile/page.tsx
+++ frontend/neumann-client/src/app/(main)/settings/profile/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import ProfileForm from "@/components/settings/profile/Form"
-import { useUser } from "@/contexts/UserContext"
-import InputLoading from "@/components/settings/profile/InputLoading"
+import ProfileForm from '@/components/settings/profile/Form'
+import InputLoading from '@/components/settings/profile/InputLoading'
+import { useUser } from '@/contexts/UserContext'
 
 export default function ProfilePage() {
   const { user, setUser } = useUser()
