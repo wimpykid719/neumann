9bc9bb8eb5414c077838c06072506a2bf812a941
wataru <ltomu.web@gmail.com>
2024-04-09 15:38:41 +0900

    fix: サーバサイドで実行されるリクエストからcredentialsを取り除く
    サーバサイドからはcookie（リフレッシュトークン）を飛ばす必要がない（飛ばせない）
    のでcredentialsを指定を取り除く

--- frontend/neumann-client/src/lib/wrappedFeatch/requests/book.ts
+++ frontend/neumann-client/src/lib/wrappedFeatch/requests/book.ts
@@ -15,14 +15,12 @@ const FOUR_HOURS = 14400
 export async function getBooks(page = 1) {
   return await fetch.get<ResponseBooks, PageParams>('/api/v1/books/', {
     params: { page },
-    credentials: 'include',
     revalidate: FOUR_HOURS,
   })
 }
 
 export async function getBook(id: string) {
   return await fetch.get<ResponseBook>(`/api/v1/books/${id}`, {
-    credentials: 'include',
     revalidate: FOUR_HOURS,
   })
 }
