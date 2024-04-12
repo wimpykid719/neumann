6720e9e9e016122f782da7b07e3dfc92b0b5dc22
wataru <ltomu.web@gmail.com>
2024-04-12 12:54:11 +0900

    fix: 3列並びの際に書籍がなるべく中央に来るようにスタイルとliタグ修正
    3列並びの際になるべく中央配置されるように修正
    ただ余白が出来てしまうしいけてない

--- frontend/neumann-client/src/app/(main)/[page]/page.tsx
+++ frontend/neumann-client/src/app/(main)/[page]/page.tsx
@@ -39,12 +39,14 @@ export default async function Index({ params }: PathsProps) {
     <section className='flex min-h-screen flex-col items-center justify-between space-y-8'>
       <div className='w-full space-y-8'>
         <Tabs navigation={booksNavigation} />
-        <div>
-          <ul className='flex flex-wrap gap-12 w-full'>
+        <div className='lg:max-w-5xl md:max-w-2xl m-auto'>
+          <ul className='flex flex-wrap sm:justify-normal justify-center lg:gap-12 md:gap-8 sm:gap-2'>
             {res.books.map((book, index) => (
-              <Link key={book.id.toString()} href={`books/${book.id}`}>
-                <Card title={book.title} img_url={book.img_url} ranking={res.rankings[index]} />
-              </Link>
+              <li key={book.id.toString()} className='sm:mb-0 mb-8'>
+                <Link href={`books/${book.id}`}>
+                  <Card title={book.title} img_url={book.img_url} ranking={res.rankings[index]} />
+                </Link>
+              </li>
             ))}
           </ul>
         </div>
--- frontend/neumann-client/src/app/(main)/page.tsx
+++ frontend/neumann-client/src/app/(main)/page.tsx
@@ -16,12 +16,14 @@ export default async function Home() {
     <section className='flex flex-col min-h-screen items-center justify-between space-y-8'>
       <div className='w-full space-y-8'>
         <Tabs navigation={booksNavigation} />
-        <div>
-          <ul className='flex flex-wrap lg:gap-12 gap-8 md:justify-normal justify-center w-full'>
+        <div className='lg:max-w-5xl md:max-w-2xl m-auto'>
+          <ul className='flex flex-wrap sm:justify-normal justify-center lg:gap-12 md:gap-8 sm:gap-2'>
             {res.books.map((book, index) => (
-              <Link key={book.id.toString()} href={`books/${book.id}`}>
-                <Card title={book.title} img_url={book.img_url} ranking={res.rankings[index]} />
-              </Link>
+              <li key={book.id.toString()} className='sm:mb-0 mb-8'>
+                <Link href={`books/${book.id}`}>
+                  <Card title={book.title} img_url={book.img_url} ranking={res.rankings[index]} />
+                </Link>
+              </li>
             ))}
           </ul>
         </div>
