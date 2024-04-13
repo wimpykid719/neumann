4caa1767f94b01fabfdbf9b850927af3f963f273
wataru <ltomu.web@gmail.com>
2024-04-13 13:21:49 +0900

    fix: 設定ページレイアウト命名、スタイル
    Rootレイアウトではないため命名の修正
    軽微なスタイルの修正を行いました

--- frontend/neumann-client/src/app/(main)/settings/layout.tsx
+++ frontend/neumann-client/src/app/(main)/settings/layout.tsx
@@ -1,14 +1,14 @@
 import Tabs from '@/components/common/Tabs'
 import { settingsNavigation } from '@/components/common/Tabs/Navigations'
 
-export default function RootLayout({
+export default function SettingsLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <section className=''>
-      <div className='text-xl font-bold h-32 flex items-center'>設定</div>
+    <section className='space-y-8'>
+      <div className='text-xl font-bold h-24 flex items-center'>設定</div>
       <Tabs navigation={settingsNavigation} />
       {children}
     </section>
