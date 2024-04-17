eef6219cffa4766d4e6d0022e2bd391ae1cb0597
wataru <ltomu.web@gmail.com>
2024-04-18 02:17:07 +0900

    fix: 書籍ページのキャッシュを無効化してISRからSSRに変更

--- frontend/neumann-client/src/lib/wrappedFeatch/requests/book.ts
+++ frontend/neumann-client/src/lib/wrappedFeatch/requests/book.ts
@@ -10,17 +10,17 @@ type ResponseBooks = {
 
 type ResponseBook = BookDetail
 
-const FOUR_HOURS = 14400
+const NO_CACHE_SSR = 0
 
 export async function getBooks(page = 1) {
   return await fetch.get<ResponseBooks, PageParams>('/api/v1/books/', {
     params: { page },
-    revalidate: FOUR_HOURS,
+    revalidate: NO_CACHE_SSR,
   })
 }
 
 export async function getBook(id: string) {
   return await fetch.get<ResponseBook>(`/api/v1/books/${id}`, {
-    revalidate: FOUR_HOURS,
+    revalidate: NO_CACHE_SSR,
   })
 }
