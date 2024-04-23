e5ea2b7b8d4311d17bdcccee115b1c60085b1a7e
wataru <ltomu.web@gmail.com>
2024-04-24 01:16:09 +0900

    fix: entrypointから初期アプリ作成コマンド削除

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,13 +5,6 @@ bundle install
 bundle exec rails db:create RAILS_ENV=test
 bundle exec rails db:migrate
 
-if [ ! -e "/backend/config/routes.rb" ]; then
-  echo 'rails new APIモード を実行する'
-  # --skip入れないとpgのgemないってエラーが出る
-  rails new . --force --api --database=mysql --skip-git --skip-bundle
-  bundle install
-fi
-
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /backend/tmp/pids/server.pid
 
