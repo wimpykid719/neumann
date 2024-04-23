6c3ed8b8aad98c60292a817d3fc982b97c04fd34
wataru <ltomu.web@gmail.com>
2024-04-24 03:24:10 +0900

    chore: デバッグ用

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -e
 
+echo 'bundle install 実行'
 bundle install
+echo 'db create 実行'
 bundle exec rails db:create RAILS_ENV=test
+echo 'db マイグレーション 実行'
 bundle exec rails db:migrate
+
+echo 'フォルダ構成'
+echo "$(ls)"
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /backend/tmp/pids/server.pid
