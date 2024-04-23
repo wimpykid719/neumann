89a3375efde507be2d977fac5ec5367e8dae4438
wataru <ltomu.web@gmail.com>
2024-04-23 11:22:45 +0900

    chore: Cloud Runのホスト名をホワイトリストに追加
    コンテナを起動する際にhttpリクエストがこのホスト名から飛んでくるため
    リクエストを受け付ける必要があるんだと思う

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
+  config.hosts << /.*\.run\.app/
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
