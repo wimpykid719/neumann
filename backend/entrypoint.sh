5fa5cbb07534b5c08b3243175722a00cbd240dd6
wataru <ltomu.web@gmail.com>
2024-04-24 15:01:30 +0900

    fix: 本番環境では不要なgemをインストールしないように変更
    develop用のDBを作成しなくて良い理由はDBのコンテナ起動時にdocker-compose側で作成しているから
    そのためテスト用のDBは再度作成する必要がある
    本番用も事前に作成してあるのでコマンドで作成する必要ない

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 set -e
 
-echo 'bundle install 実行'
-bundle install
-echo 'db create 実行'
-bundle exec rails db:create RAILS_ENV=test
-echo 'db マイグレーション 実行'
+# 本番環境以外で実行された場合はテストDBを作成
+if [ "$RAILS_ENV" != "production" ]; then
+  echo '開発環境用のGemをインストール'
+  bundle install
+
+  echo 'RSpec用のテストDBを作成'
+  bundle exec rails db:create RAILS_ENV=test
+
+else
+  echo '本番環境用のGemをインストール'
+  bundle install --without development test
+fi
+
 bundle exec rails db:migrate
 
-echo 'フォルダ構成'
-echo "$(ls)"
+if [ ! -e "/backend/config/routes.rb" ]; then
+  echo 'rails new APIモード を実行する'
+  # --skip入れないとpgのgemないってエラーが出る
+  rails new . --force --api --database=mysql --skip-git --skip-bundle
+  bundle install
+fi
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /backend/tmp/pids/server.pid
