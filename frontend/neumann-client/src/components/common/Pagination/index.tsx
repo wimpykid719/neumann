b8173883c8d0438867e9be2771db8ac10b1fdba1
wataru <ltomu.web@gmail.com>
2024-04-25 11:52:14 +0900

    feat: ページネーションレスポンシブ化

--- frontend/neumann-client/src/components/common/Pagination/index.tsx
+++ frontend/neumann-client/src/components/common/Pagination/index.tsx
@@ -1,156 +1,21 @@
 'use client'
 
-import { FIRST_PAGE } from '@/utils/page'
-import { range } from '@/utils/range'
-import Ellipsis from './Ellipsis'
-import Navigation from './Navigation'
-import Number from './Number'
+import { useDeviceWindow } from '@/hooks/useDeviceWindow'
+import Base from './Base'
+import { PaginationProps } from './Base'
 
-type PaginationProps = {
-  path?: string
-  page: number
-  lastPage: number
-  disabled?: boolean
-  // ページネーションの両端に絶対表示しておきたい数値の数
-  // 例：1の場合で最終ページが10の場合 1,10が常に両端に表示される。2の場合は1 2, 9 10
-  // が常に表示される
-  boundaryCount?: number
-  siblingCount?: number
-  hideNextButton?: boolean
-  hidePrevButton?: boolean
-  showFirstButton?: boolean
-  showLastButton?: boolean
+type ResponsiblePagination = {
+  page: PaginationProps['page']
+  lastPage: PaginationProps['lastPage']
+  siblingCount: PaginationProps['siblingCount']
 }
-
-type PageType = 'first' | 'previous' | 'next' | 'last' | 'start-ellipsis' | 'end-ellipsis'
 
 export const INITIAL_PAGE = 1
 
-export default function Pagination({
-  path,
-  page,
-  lastPage,
-  disabled = false,
-  boundaryCount = 1,
-  siblingCount = 1,
-  hideNextButton = false,
-  hidePrevButton = false,
-  showFirstButton = false,
-  showLastButton = false,
-}: PaginationProps) {
-  const startPages = range(1, Math.min(boundaryCount, lastPage))
-  const endPages = range(Math.max(lastPage - boundaryCount + 1, boundaryCount + 1), lastPage)
+export default function ResponsiblePagination({ page, lastPage, siblingCount }: ResponsiblePagination) {
+  const SM = 640
+  const SMALL_DEVICE_SIBLING = 0
+  const device = useDeviceWindow()
 
-  const siblingsStart = Math.max(
-    Math.min(
-      // Natural start
-      page - siblingCount,
-      // Lower boundary when page is high
-      lastPage - boundaryCount - siblingCount * 2 - 1,
-    ),
-    // Greater than startPages
-    boundaryCount + 2,
-  )
-
-  const siblingsEnd = Math.min(
-    Math.max(
-      // Natural end
-      page + siblingCount,
-      // Upper boundary when page is low
-      boundaryCount + siblingCount * 2 + 2,
-    ),
-    // Less than endPages
-    endPages.length > 0 ? endPages[0] - 2 : lastPage - 1,
-  )
-
-  // Basic list of items to render
-  // e.g. itemList = ['first', 'previous', 1, 'ellipsis', 4, 5, 6, 'ellipsis', 10, 'next', 'last']
-  const itemList = [
-    ...(showFirstButton ? ['first' as const] : []),
-    ...(hidePrevButton ? [] : ['previous' as const]),
-    ...startPages,
-
-    // Start ellipsis
-    // eslint-disable-next-line no-nested-ternary
-    ...(siblingsStart > boundaryCount + 2
-      ? ['start-ellipsis' as const]
-      : boundaryCount + 1 < lastPage - boundaryCount
-        ? [boundaryCount + 1]
-        : []),
-
-    // Sibling pages
-    ...range(siblingsStart, siblingsEnd),
-
-    // End ellipsis
-    // eslint-disable-next-line no-nested-ternary
-    ...(siblingsEnd < lastPage - boundaryCount - 1
-      ? ['end-ellipsis' as const]
-      : lastPage - boundaryCount > boundaryCount
-        ? [lastPage - boundaryCount]
-        : []),
-
-    ...endPages,
-    ...(hideNextButton ? [] : ['next' as const]),
-    ...(showLastButton ? ['last' as const] : []),
-  ]
-
-  const itemDom = (type: PageType) => {
-    const buttonDisabled =
-      disabled ||
-      (type.indexOf('ellipsis') === -1 && (type === 'next' || type === 'last' ? page >= lastPage : page <= 1))
-
-    switch (type) {
-      case 'first':
-        return (
-          <Number
-            path={path}
-            page={FIRST_PAGE}
-            selected={1 === page}
-            disabled={disabled}
-            ariaCurrent={1 === page ? 'true' : undefined}
-          />
-        )
-      case 'previous':
-        return <Navigation icon={<>{'<'}</>} page={page - 1} disabled={buttonDisabled} />
-      case 'next':
-        return <Navigation icon={<>{'>'}</>} page={page + 1} disabled={buttonDisabled} />
-      case 'last':
-        return (
-          <Number
-            path={path}
-            page={lastPage}
-            selected={lastPage === page}
-            disabled={disabled}
-            ariaCurrent={lastPage === page ? 'true' : undefined}
-          />
-        )
-      default:
-        return <Ellipsis />
-    }
-  }
-
-  // Convert the basic item list to PaginationItem props objects
-  const items = itemList.map(item => {
-    return typeof item === 'number' ? (
-      <Number
-        path={path}
-        page={item}
-        selected={item === page}
-        disabled={disabled}
-        ariaCurrent={item === page ? 'true' : undefined}
-      />
-    ) : (
-      itemDom(item)
-    )
-  })
-
-  return (
-    <div className='flex justify-center flex-col items-center w-full'>
-      <ul className='flex flex-wrap space-x-3 justify-center items-center max-w-96 w-full'>
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  )
+  return <Base page={page} lastPage={lastPage} siblingCount={device.width < SM ? SMALL_DEVICE_SIBLING : siblingCount} />
 }
