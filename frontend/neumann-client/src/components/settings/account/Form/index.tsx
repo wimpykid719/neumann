ffea9b725d3089bb472463916e63830008504ed4
wataru <ltomu.web@gmail.com>
2024-04-26 18:19:23 +0900

    fix: 不要なスタイル削除

--- frontend/neumann-client/src/components/settings/account/Form/index.tsx
+++ frontend/neumann-client/src/components/settings/account/Form/index.tsx
@@ -74,7 +74,7 @@ export default function AccountForm({ user, setUser }: Props) {
 
   return (
     <section className='flex flex-col items-center justify-center px-6 mx-auto'>
-      <div className='w-full md:mt-0 lg:max-w-xl sm:max-w-md xl:p-0'>
+      <div className='w-full md:mt-0 lg:max-w-xl sm:max-w-md'>
         <div className='lg:p-6 space-y-4 md:space-y-6 sm:p-8'>
           <div>
             <span className='block mb-2 text-sm font-medium'>ユーザ名</span>
