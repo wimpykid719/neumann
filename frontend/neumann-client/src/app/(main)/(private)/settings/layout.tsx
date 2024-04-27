4302f79249cc37b845635c818abe5ee4d57c1259
wataru <ltomu.web@gmail.com>
2024-04-27 22:03:34 +0900

    fix: アカウント設定のスタイル

--- frontend/neumann-client/src/app/(main)/(private)/settings/layout.tsx
+++ frontend/neumann-client/src/app/(main)/(private)/settings/layout.tsx
@@ -8,7 +8,7 @@ export default function SettingsLayout({
   children: React.ReactNode
 }>) {
   return (
-    <section>
+    <section className='m-auto w-full max-w-5xl space-y-8 md:px-14 px-8 pt-8'>
       <Header />
       <div className='space-y-8'>
         <div className='text-xl font-bold h-24 flex items-center'>設定</div>
