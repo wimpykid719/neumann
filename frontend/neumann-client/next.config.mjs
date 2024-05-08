ba69f1b0372a078e2a295e730aa812f715a27fb7
wataru <ltomu.web@gmail.com>
2024-05-08 12:29:45 +0900

    chore: googleのプロフィール画像が表示可能

--- frontend/neumann-client/next.config.mjs
+++ frontend/neumann-client/next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
         port: '',
         pathname: '/images/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh*.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   output: 'standalone',
