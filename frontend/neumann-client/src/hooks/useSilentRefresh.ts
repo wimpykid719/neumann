2a4bbb40cd2e1e8f9fe3b168566e10bb8c33f750
wataru <ltomu.web@gmail.com>
2024-05-20 17:38:15 +0900

    fix: サイレントリフレッシュ失敗した際、ログインボタンが表示されない

--- frontend/neumann-client/src/hooks/useSilentRefresh.ts
+++ frontend/neumann-client/src/hooks/useSilentRefresh.ts
@@ -1,19 +1,24 @@
 import { useAccessToken } from '@/contexts/AccessTokenContext'
+import { useLoginHistory } from '@/contexts/LoginHistoryContext'
 import { FetchError } from '@/lib/errors'
 import { silentRefresh } from '@/lib/wrappedFeatch/silentRefresh'
 import { ToastType } from '@/types/toast'
+import { deleteLogoutStatus } from '@/utils/localStorage'
 import { toastStatus } from '@/utils/toast'
 import { useEffect, useState } from 'react'
 
 export const useSilentRefresh = (showToast: (message: string, type: ToastType) => void) => {
   const { accessToken, setAccessToken } = useAccessToken()
   const [isRefreshed, setIsRefreshed] = useState(false)
+  const { setLoginHistory } = useLoginHistory()
 
   const execSilentRefresh = async () => {
     const res = await silentRefresh(accessToken)
     if (res instanceof FetchError) {
       setIsRefreshed(true)
       showToast(res.message, toastStatus.error)
+      setLoginHistory(null)
+      deleteLogoutStatus()
     } else {
       setIsRefreshed(true)
       if (res) {
