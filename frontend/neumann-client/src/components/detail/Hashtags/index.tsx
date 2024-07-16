76cf1c18a1982c66ba109e489b1275157187150e
wataru <ltomu.web@gmail.com>
2024-07-17 01:45:43 +0900

    feat: ハッシュタグが20より大きい場合、もっと見るを表示
    Netflixのwebページを参考に要素ともっと見るボタンがグレデーションを背景に重なるUIを実装しました
    z-indexを使用しない場合に要素に重なりが発生するとrelativeを設定した要素が上に来る仕様を使って実装しました
    https://developer.mozilla.org/ja/docs/Web/CSS/CSS_positioned_layout/Understanding_z-index/Stacking_without_z-index

--- frontend/neumann-client/src/components/detail/Hashtags/index.tsx
+++ frontend/neumann-client/src/components/detail/Hashtags/index.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import { BookDetail } from '@/types/book'
+import { useState } from 'react'
 
 type HashtagsProps = {
   hashtags: BookDetail['note_reference']['hashtags']
@@ -9,15 +12,37 @@ const removeHashtag = (str: string) => str.replace(/^#/, '')
 const noteHashtagUrl = (tag: string) => 'https://note.com/hashtag/' + removeHashtag(tag)
 
 export default function Hashtags({ hashtags }: HashtagsProps) {
+  const DISPLAY_COLLAPSE_LENGTH = 20
+  const [isCollapse, setCollapse] = useState(DISPLAY_COLLAPSE_LENGTH < hashtags.length)
+
   return (
-    <ul className='flex items-start flex-wrap'>
-      {hashtags.map(tag => (
-        <li className='mx-2 mb-4 border rounded-lg text-xs  text-center hover:opacity-70 border-gray-500 text-gray-500'>
-          <a href={noteHashtagUrl(tag)} target='_blank' className='block px-2 py-2' rel='noreferrer'>
-            {tag}
-          </a>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul className={`flex items-start flex-wrap ${isCollapse && 'overflow-hidden max-h-40 lg:max-h-56'}`}>
+        {hashtags.map(tag => (
+          <li
+            key={tag}
+            className='mx-2 mb-4 border rounded-lg text-xs text-center hover:opacity-70 border-gray-500 text-gray-500'
+          >
+            <a href={noteHashtagUrl(tag)} target='_blank' className='block px-2 py-2' rel='noreferrer'>
+              {tag}
+            </a>
+          </li>
+        ))}
+      </ul>
+      {isCollapse && (
+        <div
+          className={`
+          bg-gradient-to-t from-gray-50 dark:from-gray-900
+          flex justify-center
+          items-end w-full relative
+          h-24 ${isCollapse && '-mt-24'}
+        `}
+        >
+          <button className='text-sm text-secondary hover:opacity-70' onClick={() => setCollapse(false)}>
+            もっと見る
+          </button>
+        </div>
+      )}
+    </div>
   )
 }
