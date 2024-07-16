c66d9f28906b084a3f833ce0010b87bfdcac5981
wataru <ltomu.web@gmail.com>
2024-07-16 14:56:15 +0900

    fix: 書籍詳細のハッシュタグクリック可能範囲
    テキスト文字をクリックする必要があったため
    タグの周囲でもクリック出来るように変更

--- frontend/neumann-client/src/components/detail/Hashtags/index.tsx
+++ frontend/neumann-client/src/components/detail/Hashtags/index.tsx
@@ -12,8 +12,8 @@ export default function Hashtags({ hashtags }: HashtagsProps) {
   return (
     <ul className='flex items-start flex-wrap'>
       {hashtags.map(tag => (
-        <li className='mx-2 mb-4 border rounded-lg text-xs px-2 py-2 text-center hover:opacity-70 border-gray-500 text-gray-500'>
-          <a href={noteHashtagUrl(tag)} target='_blank' rel='noreferrer'>
+        <li className='mx-2 mb-4 border rounded-lg text-xs  text-center hover:opacity-70 border-gray-500 text-gray-500'>
+          <a href={noteHashtagUrl(tag)} target='_blank' className='block px-2 py-2' rel='noreferrer'>
             {tag}
           </a>
         </li>
