593000296e52541ee6cb4919cfa7fb45d0d1146c
wataru <ltomu.web@gmail.com>
2024-08-03 08:42:29 +0900

    feat: UserAnalyticモデル追加

--- backend/lib/constants/regexps.rb
+++ backend/lib/constants/regexps.rb
@@ -6,4 +6,5 @@ class Constants::Regexps
   TIKTOK = /\A([^@].*|)\z/
   YOUTUBE = /\A([^@].*|)\z/
   DATE = /\A\z|\A[1-9]\d{3}-\d{2}-\d{2}\z/
+  MONTH_DAY = %r{\A(0[1-9]|1[0-2])/(0[1-9]|[12]\d|3[01])\z}
 end
