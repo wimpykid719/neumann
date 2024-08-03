e0c8a33abc66d5003f7423d06abda6a6d91a1db1
wataru <ltomu.web@gmail.com>
2024-08-03 23:33:32 +0900

    feat: フッターのリンク設定

--- frontend/neumann-client/src/components/common/Footer/index.tsx
+++ frontend/neumann-client/src/components/common/Footer/index.tsx
@@ -6,6 +6,11 @@ import BizRankIcon from '../icon/BizRankIcon'
 const DynamicUsersChart = dynamic(() => import('./UsersChart'), { ssr: false })
 
 export default function Footer() {
+  const BIZRANK_X = 'https://x.com/biz_rank57580'
+  const DEFECT_REPORT = 'https://forms.gle/SthcCR8ELKGQ5Bgt7'
+  const INQUIRY = 'https://forms.gle/3VHsnYvQUDHsTgUh7'
+  const FEATURE = 'https://github.com/users/wimpykid719/projects/2'
+
   return (
     <div className='sub-bg-color'>
       <div className='lg:flex max-w-5xl mx-auto py-12 px-4 text-xs lg:space-y-0 space-y-8'>
@@ -28,15 +33,31 @@ export default function Footer() {
                 <li key='aboutsite'>
                   <Link href={'/policies/about'}>このサイトについて</Link>
                 </li>
-                <li key='feature'>予定する機能追加について</li>
+                <li key='feature'>
+                  <a href={FEATURE} target='_blank' rel='nofollow'>
+                    予定する機能追加について
+                  </a>
+                </li>
               </ul>
             </div>
             <div className='lg:max-w-40 max-w-28 w-full space-y-5'>
               <div className='font-bold'>Links</div>
               <ul className='space-y-4'>
-                <li key='official-x'>X（Twitter）</li>
-                <li key='defect-report'>不具合報告</li>
-                <li key='inquiry'>問い合わせ</li>
+                <li key='official-x'>
+                  <a href={BIZRANK_X} target='_blank' rel='nofollow'>
+                    X（Twitter）
+                  </a>
+                </li>
+                <li key='defect-report'>
+                <a href={DEFECT_REPORT} target='_blank' rel='nofollow'>
+                  不具合報告
+                </a>
+                </li>
+                <li key='inquiry'>
+                <a href={INQUIRY} target='_blank' rel='nofollow'>
+                  問い合わせ
+                </a>
+                </li>
               </ul>
             </div>
           </div>
