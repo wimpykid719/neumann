d28287c869d9f5d1229790ee7e308c86f415a0eb
wataru <ltomu.web@gmail.com>
2024-08-06 15:44:28 +0900

    chore: いいね数、発見を非表示
    APIの実装がまだなため非表示とする

--- frontend/neumann-client/src/components/common/Tabs/Navigations.ts
+++ frontend/neumann-client/src/components/common/Tabs/Navigations.ts
@@ -11,8 +11,8 @@ export const policiesNavigation = [
 
 export const booksNavigation = [
   { name: '話題 ( •̀ᴗ•́ )و ̑̑ ', key: 'hot', href: '/' },
-  { name: 'いいね数  (｡♥ ‿  ♥｡)', key: 'likes', href: '#' },
-  { name: '発見 (ﾉﾟοﾟ)ﾉ', key: 'new', href: '#' },
+  // { name: 'いいね数  (｡♥ ‿  ♥｡)', key: 'likes', href: '#' },
+  // { name: '発見 (ﾉﾟοﾟ)ﾉ', key: 'new', href: '#' },
 ]
 
 export const bookNavigation = [{ name: '本について', key: 'about', href: undefined }]
