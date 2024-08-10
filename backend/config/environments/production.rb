1bf8dac88c9067a136d9f3b91b65f248360e7f8f
wataru <ltomu.web@gmail.com>
2024-08-10 16:34:13 +0900

    chore: cors・hostsにカスタムドメインを追加

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -84,6 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Allow Cloud Run domains
-  config.hosts << /.*\.run\.app/
+  config.hosts << ENV.fetch('BASE_URL')
 end
