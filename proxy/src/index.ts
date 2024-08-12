eaae848a90dba00dab9d5cc9a72d8f55f628cc9d
wataru <ltomu.web@gmail.com>
2024-08-12 21:12:08 +0900

    fix: cloudflare workers用にreverse proxyを書き直す

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -1,25 +1,40 @@
-import { createServer, request } from 'http'
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
 
-const isApiPath = (path: string | undefined) => !!path && path.startsWith('/api/')
+export interface Env {
+  BACKEND_URL: string
+  FRONTEND_URL: string
+}
 
-createServer((clientReq, clientRes) => {
-  const path = clientReq.url
-  const isApiRequest = isApiPath(path)
-  // クライアントから受け取ったリクエストを、バックエンドサーバに送る処理
-  const serverReq = request(isApiRequest ? process.env.BACKEND_URL! : process.env.FRONTEND_URL!, {
-    method: clientReq.method,
-    path,
-    headers: clientReq.headers,
-  })
-    .on('error', () => clientRes.writeHead(502).end()) // バックエンドサーバとの通信エラーが発生した場合
-    .on('timeout', () => clientRes.writeHead(504).end()) // バックエンドサーバとの通信がタイムアウトした場合
-    .on('response', serverRes => {
-      // バックエンドから受け取ったレスポンスをクライアントに送る処理
-      // ステータスコードやヘッダをそのまま送る
-      clientRes.writeHead(serverRes.statusCode!, serverRes.headers)
-      // HTTPボディはストリームパイプで流す
-      serverRes.pipe(clientRes)
+export default {
+  async fetch(request, env, ctx): Promise<Response> {
+    const isApiPath = (path: string | undefined) => !!path && path.startsWith('/api/')
+
+    const url = new URL(request.url)
+    const isApiRequest = isApiPath(url.pathname)
+
+    const baseUrl = isApiRequest ? env.BACKEND_URL : env.FRONTEND_URL
+    const targetUrl = new URL(url.pathname + url.search, baseUrl)
+    const modifiedRequest = new Request(targetUrl, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
     })
-  // リクエストのHTTPボディはストリームパイプで流す
-  clientReq.pipe(serverReq)
-}).listen(process.env.PROXY_PORT) // リバースプロキシサーバのポート
+
+    try {
+      return await fetch(modifiedRequest)
+    } catch (error) {
+      return new Response('Bad Gateway', { status: 502 })
+    }
+  },
+} satisfies ExportedHandler<Env>
