25c0abc2994856cb0d9376d61f5cd2653bcbf7ea
wataru <ltomu.web@gmail.com>
2024-08-15 00:07:28 +0900

    refactor: ページコンポーネントの命名規則に従う
    アッパーキャメルケースで定義

--- frontend/neumann-client/src/app/(main)/(public)/policies/privacy/page.tsx
+++ frontend/neumann-client/src/app/(main)/(public)/policies/privacy/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: app.privacy,
 }
 
-export default async function privacyPage() {
+export default async function PrivacyPage() {
   const markdown = readMarkdown('src/md/privacy.md')
 
   return (
