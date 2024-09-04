031f01cf4236dcc764c41bbaa8683e60de79297c
wataru <ltomu.web@gmail.com>
2024-09-04 21:08:32 +0900

    chore: noteで取得する記事のハッシュタグ追加

--- bot/src/utils/hashTags.ts
+++ bot/src/utils/hashTags.ts
@@ -1,2 +1,11 @@
-export const HASH_TAGS = ['ビジネス書評', 'おすすめのビジネス書', 'ビジネス書レビュー', 'ビジネス書'] as const
+export const HASH_TAGS = [
+  'ビジネス書評',
+  'おすすめのビジネス書',
+  'ビジネス書レビュー',
+  'ビジネス書',
+  'キャリア',
+  'ビジネススキル',
+  'マーケティング',
+  '起業',
+] as const
 export type HashTags = (typeof HASH_TAGS)[number]
